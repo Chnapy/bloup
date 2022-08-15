d00549c99ca70018fdc1985d7510f0cb0a86cc38
Richard Haddad <richardhaddad@hotmail.fr>
2022-08-15 15:32:12 +0000

    minor refactor

--- packages/api/src/auth/auth.guard.ts
+++ packages/api/src/auth/auth.guard.ts
@@ -1,4 +1,8 @@
-import { ExecutionContext, Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { GraphQLContext } from '../config/data.module';
@@ -8,10 +12,8 @@ import { ROLES_KEY } from './roles.decorator';
 
 @Injectable()
 export class AuthGuard extends authGuardGoogle {
-  constructor(
-    private reflector: Reflector
-  ) {
-    super()
+  constructor(private reflector: Reflector) {
+    super();
   }
 
   getResponse(context: ExecutionContext) {
@@ -27,22 +29,25 @@ export class AuthGuard extends authGuardGoogle {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const canActivate = await super.canActivate(context);
 
-    if(!canActivate) {
+    if (!canActivate) {
       return canActivate;
     }
 
-    const requiredRoles = this.reflector.getAllAndOverride<UserRole[] | undefined>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]) ?? [];
+    const requiredRoles =
+      this.reflector.getAllAndOverride<UserRole[] | undefined>(ROLES_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]) ?? [];
 
-    if(requiredRoles.length === 0) {
-      throw new InternalServerErrorException('At least one role must be specified using @Roles() decorator.');
+    if (requiredRoles.length === 0) {
+      throw new InternalServerErrorException(
+        'At least one role must be specified using @Roles() decorator.'
+      );
     }
 
-    const {user} = this.getRequest(context);
+    const { user } = this.getRequest(context);
     const userRoles = user?.roles ?? [];
-    
+
     return requiredRoles.some((role) => userRoles.includes(role));
   }
 }
--- packages/api/src/config/data.module.ts
+++ packages/api/src/config/data.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServerResponse } from 'node:http';
+import { DataSourceOptions } from 'typeorm';
 import type { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOptions';
 import { RequestWithUser } from '../auth/google.strategy';
 
@@ -13,23 +14,23 @@ export type GraphQLContext = {
   res: ServerResponse;
 };
 
+export const dataSourceOptions: DataSourceOptions = {
+  url: process.env.DB_URL,
+  type: 'mongodb',
+  entities: ['src/**/*.entity.js'],
+  synchronize: isDev,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  logging: true,
+  // cache: true,
+};
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      url: process.env.DB_URL,
-      type: 'mongodb',
-      entities: ['../**/*.entity.js'],
+      ...dataSourceOptions,
       autoLoadEntities: true,
-      synchronize: isDev,
-      cli: {
-        entitiesDir: '../**/entities',
-        migrationsDir: 'migrations',
-        subscribersDir: 'subscribers',
-      },
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       keepConnectionAlive: true,
-      logging: true,
     } as MongoConnectionOptions),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
